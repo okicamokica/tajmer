0250d4a89b679186538dd806ab38715be739449b
okicamokica <zoran000@outlook.com>
2026-02-26 20:02:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,19 @@
-const cacheName = 'mon-v1';
+const cacheName = 'monika-v2';
 const staticAssets = ['./', './index.html'];
 
-self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
-  return self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(staticAssets)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+// Slušanje poruke za slanje obavijesti
+self.addEventListener('message', event => {
+  if (event.data.type === 'SHOW_NOTIFICATION') {
+    self.registration.showNotification(event.data.title, {
+      body: event.data.body,
+      icon: 'https://cdn-icons-png.flaticon.com/512/1046/1046747.png',
+      vibrate: [200, 100, 200],
+      tag: 'timer-notification', // Spriječava gomilanje obavijesti
+      renotify: true
+    });
+  }
 });
