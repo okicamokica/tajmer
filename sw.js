6fbc8953555d5e9d832c97caa7984927aea33db6
okicamokica <zoran000@outlook.com>
2026-02-26 19:46:07 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const cacheName = 'mon-v1';
+const staticAssets = ['./', './index.html'];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
